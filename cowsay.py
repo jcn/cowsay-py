b99ccdbc337ce91b58d2c46b64923a1bc7620903
Jesse Chan-Norris <jcn@pith.org>
2011-12-20 17:59:24 -0500

    Add LGPL licensing

--- cowsay.py
+++ cowsay.py
@@ -1,5 +1,6 @@
 # A python implementation of cowsay <http://www.nog.net/~tony/warez/cowsay.shtml>
 # Copyright 2011 Jesse Chan-Norris <jcn@pith.org>
+# Licensed under the GNU LGPL version 3.0
 
 import sys
 import textwrap

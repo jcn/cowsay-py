766ee9d20d5c635a25716ec74635d6d1d50f6f21
evgeniygalagurya <evgeniygalagurua@gmail.com>
2023-01-29 18:50:21 +0200

    repo:change

--- cowsay.py
+++ cowsay.py
@@ -1,63 +1,100 @@
-# A python implementation of cowsay <http://www.nog.net/~tony/warez/cowsay.shtml>
-# Copyright 2011 Jesse Chan-Norris <jcn@pith.org>
-# Licensed under the GNU LGPL version 3.0
-
 from __future__ import print_function
 import sys
-import textwrap
+import re
 
-def cowsay(str, length=40):
-    return build_bubble(str, length) + build_cow()
+from .characters import CHARS
 
-def build_cow():
-    return """
-         \   ^__^ 
-          \  (oo)\_______
-             (__)\       )\/\\\\
-                 ||----w |
-                 ||     ||
-    """
+__version__ = '5.0'
 
-def build_bubble(str, length=40):
-    bubble = []
+char_names = list(CHARS.keys())
 
-    lines = normalize_text(str, length)
 
-    bordersize = len(lines[0])
+def wrap_lines(lines, max_width=49):
+    new_lines = []
+    for line in lines:
+        for line_part in [
+            line[i:i + max_width] for i in range(0, len(line), max_width)
+        ]:
+            new_lines.append(line_part)
+    return new_lines
 
-    bubble.append("  " + "_" * bordersize)
 
-    for index, line in enumerate(lines):
-        border = get_border(lines, index)
+def generate_bubble(text):
+    lines = [line.strip() for line in str(text).split("\n")]
+    lines = wrap_lines([line for line in lines if line])
+    text_width = max([len(line) for line in lines])
+    output = []
+    output.append("  " + "_" * text_width)
+    if len(lines) > 1:
+        output.append(" /" + " " * text_width + "\\")
+    for line in lines:
+        output.append("| " + line + " " * (text_width - len(line) + 1) + "|")
+    if len(lines) > 1:
+        output.append(" \\" + " " * text_width + "/")
+    output.append("  " + "=" * text_width)
+    return output
 
-        bubble.append("%s %s %s" % (border[0], line, border[1]))
 
-    bubble.append("  " + "-" * bordersize)
+def generate_char(char, text_width):
+    output = []
+    char_lines = char.split('\n')
+    char_lines = [i for i in char_lines if len(i) != 0]
+    for line in char_lines:
+        output.append(' ' * text_width + line)
+    return output
 
-    return "\n".join(bubble)
 
-def normalize_text(str, length):
-    lines  = textwrap.wrap(str, length)
-    maxlen = len(max(lines, key=len))
-    return [ line.ljust(maxlen) for line in lines ]
+# we are doing some magic here: Creating the functions dynamically.
+# in .characters is a dict CHARS which holds the lines for the characters.
+# For each entry there, we create a function.
+# Wo do this, to not break the old API.
 
-def get_border(lines, index):
-    if len(lines) < 2:
-        return [ "<", ">" ]
+def draw(char, text, to_console=True):
+    if len(re.sub('\s', '', text)) == 0:
+        raise Exception('Pass something meaningful to cowsay')
+    output = generate_bubble(text)
+    text_width = max([len(line) for line in output]) - 4  # 4 is the frame
+    output += generate_char(char, text_width)
+    if to_console:
+        for line in output:
+            print(line)
+    return '\n'.join(output)
 
-    elif index == 0:
-        return [ "/", "\\\\" ]
-    
-    elif index == len(lines) - 1:
-        return [ "\\\\", "/" ]
-    
+
+chars = {}
+for char_name, char_lines in CHARS.items():
+    def func(text, char_lines=char_lines):
+        draw(char_lines, text)
+
+
+    func.__name__ = char_name
+    globals()[char_name] = func
+    chars[char_name] = func
+
+
+def get_output_string(char_name, text):
+    if char_name in CHARS:
+        return draw(CHARS[char_name], text, to_console=False)
     else:
-        return [ "|", "|" ]
+        raise Exception('Available Characters:', list(CHARS.keys()))
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print ("Usage: '%s string'" % sys.argv[0])
-        sys.exit(0)
+def cli():
+    if '--version' in sys.argv[1:]:
+        print(__version__)
+        exit(0)
 
-    print (cowsay(sys.argv[1]))
+    if '--character' in sys.argv[1:]:
+        character_index = sys.argv.index('--character')
+        try:
+            character = globals()[sys.argv[character_index + 1]]
+            del sys.argv[character_index: character_index + 2]
+        except (KeyError, IndexError):
+            options = ', '.join(char_names)
+            raise LookupError(
+                'Invalid character selection passed. Available options: ' + options
+            )
+    else:
+        character = cow
+
+    character(' '.join(sys.argv[1:]))
